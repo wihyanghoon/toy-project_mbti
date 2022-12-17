0dcd1a108ad7f8ecb6dd9a7158bca8814210b642
wihyanghoon <gidgns1995@gmail.com>
2022-12-17 17:12:06 +0900

    공유하기 테스트

--- js/share.js
+++ js/share.js
@@ -1,6 +1,7 @@
 const url = 'https://wihyanghoon.github.io/toy-project_mbti/'
 
 function setShare(){
+    console.log(infoList[0].name)
     var resultImg = document.querySelector('.result_img');
     var resultAlt = resultImg.firstElementChild.alt;
     const shareTitle = '십이간지 연애유형 결과';
