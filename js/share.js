8dfa8bc935997cc69c4dcd71d3ee0e6110fbac4d
wihyanghoon <gidgns1995@gmail.com>
2022-12-17 18:19:55 +0900

    css 수정 및 테스트

--- js/share.js
+++ js/share.js
@@ -1,31 +1,31 @@
 const url = 'https://wihyanghoon.github.io/toy-project_mbti/'
 
-function setShare(){
+function setShare() {
     const resultImg = document.querySelector('.result_img')
-    const resultAlt = resultImg.firstElementChild.alt; 
+    const resultAlt = resultImg.firstElementChild.alt;
     const shareTitle = '재벌집 막내아들 환생 테스트 결과';
     const shareDes = infoList[resultAlt].name;
-    const shareImage =  `${url}img/img-${resultAlt}.jpeg`
+    const shareImage = `${url}img/img-${resultAlt}.jpeg`
     const shareURL = `${url}page/result-${resultAlt}.html`
     Kakao.Share.sendDefault({
         objectType: 'feed',
         content: {
-          title: shareTitle,
-          description: shareDes,
-          imageUrl: shareImage,
-          link: {
-            mobileWebUrl: shareURL,
-            webUrl: shareURL
-          },
+            title: shareTitle,
+            description: shareDes,
+            imageUrl: shareImage,
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL
+            },
         },
         buttons: [
-          {
-            title: '웹으로 이동',
-            link: {
-              mobileWebUrl: shareURL,
-              webUrl: shareURL,
+            {
+                title: '참여하기',
+                link: {
+                    mobileWebUrl: shareURL,
+                    webUrl: shareURL,
+                },
             },
-          },
         ],
-      });
-  }
+    });
+}
