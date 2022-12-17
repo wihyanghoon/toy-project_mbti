d8dbfe784a9163a14d9638f86faf8b889246796a
wihyanghoon <gidgns1995@gmail.com>
2022-12-17 16:59:11 +0900

    공유하기 테스트

--- js/share.js
+++ js/share.js
@@ -1,24 +1,31 @@
-function kakaoShare() {
+function setShare(){
+    var resultImg = document.querySelector('.result_img');
+    var resultAlt = resultImg.firstElementChild.alt;
+    const shareTitle = '십이간지 연애유형 결과';
+    const shareDes = infoList[resultAlt].name;
+    const shareImage = url + 'img/image-' + resultAlt + '.png';
+    const shareURL = url + 'page/result-' + resultAlt + '.html';
+  
     Kakao.Link.sendDefault({
-        objectType: 'feed',
-        content: {
-          title: "fdfd",
-          description: "dfdfd",
-          imageUrl: "dfdfd",
+      objectType: 'feed',
+      content: {
+        title: shareTitle,
+        description: shareDes,
+        imageUrl: shareImage,
+        link: {
+          mobileWebUrl: shareURL,
+          webUrl: shareURL
+        },
+      },
+  
+      buttons: [
+        {
+          title: '결과확인하기',
           link: {
-            mobileWebUrl: "fdfd",
-            webUrl: "dfdfdfd"
+            mobileWebUrl: shareURL,
+            webUrl: shareURL,
           },
         },
-    
-        buttons: [
-          {
-            title: '결과확인하기',
-            link: {
-              mobileWebUrl: "fdfd",
-              webUrl: "dfdfdfd",
-            },
-          },
-        ]
-      });
-}
+      ]
+    });
+  }
