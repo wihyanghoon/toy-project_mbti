f07b74ffea61d441d7f1742ddcb9a1e78cac7f6e
wihyanghoon <gidgns1995@gmail.com>
2022-12-17 17:39:42 +0900

    기능 테스트

--- js/share.js
+++ js/share.js
@@ -1,23 +1,17 @@
 const url = 'https://wihyanghoon.github.io/toy-project_mbti/'
 
 function setShare(){
-    console.log(infoList[0].name)
-    var resultImg = document.querySelector('.result_img');
-    var resultAlt = resultImg.firstElementChild.alt;
-    const shareTitle = '십이간지 연애유형 결과';
-    const shareDes = infoList[0].name;
-    const shareImage = url + 'img/img-' + 0 + '.jpeg';
-    const shareURL = url + 'page/result-' + 0 + '.html';
+    
   
     Kakao.Link.sendDefault({
       objectType: 'feed',
       content: {
-        title: shareTitle,
-        description: shareDes,
-        imageUrl: shareImage,
+        title: "dfdfd",
+        description: "dfdfd",
+        imageUrl: "dfdfd",
         link: {
-          mobileWebUrl: shareURL,
-          webUrl: shareURL
+          mobileWebUrl: "dfdfd",
+          webUrl: "dfdfd"
         },
       },
   
@@ -25,8 +19,8 @@ function setShare(){
         {
           title: '결과확인하기',
           link: {
-            mobileWebUrl: shareURL,
-            webUrl: shareURL,
+            mobileWebUrl: "dfdfd",
+            webUrl: "dfdfd",
           },
         },
       ]
